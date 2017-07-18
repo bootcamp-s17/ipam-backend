306bf94bfff38dbe486a2d0c64e34c19311e194c
Robert Bryan <bryarobert@gmail.com>
2017-07-18 08:37:30 -0400

    equipments seeder start

--- database/seeds/EquipmentsTableSeeder.php
+++ database/seeds/EquipmentsTableSeeder.php
@@ -11,7 +11,23 @@ class EquipmentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('rooms')->insert([
+            'name' => "BLM-Sign-217",
+            'equipment_types_id' => 2,
+            'model' => '',
+            'driver' => '',
+            'serial_number' => '',
+            'ip_address' => 10.34.138.33,
+            'site_id' => 1,
+            'host_name' => 'bobby',
+            'port_number' => 80,
+            'mac_address' => 00-01-80-76-bf-9c,
+            'mab' => false,
+            'printer_Server' => '',
+            'printer_name' => ,
+            'share_name' => ,
+            'share_comment' => ,
+        ]);
     }
 }
 
