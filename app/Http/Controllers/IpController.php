e6a374a746cb8be0bfee4df5b8818ee481736fa0
Daebrion Hopwood <daebrion.hopwood.other@gmail.com>
2017-07-19 13:51:51 -0400

    next function in ipController functional/added comments

--- app/Http/Controllers/IpController.php
+++ app/Http/Controllers/IpController.php
@@ -43,11 +43,51 @@ class IpController extends Controller
 
     public function next($subnet_id)
     {
+        //Use the index function declared on line 16
+        //Within this function. 
         $ip_addresses = $this->index();
 
+        //Query the subnets table for the given id
+        //This returns an array
         $subnet = Subnet::find($subnet_id)->subnet_address;
-        
-        return $subnet;
+
+        //Extracts first 3 number fields of subnet address
+        //Pop removes the last element of the given array
+        $subnet_array = explode('.', $subnet);
+        array_pop($subnet_array);
+
+        //Implode takes the array and turns it back into a string.
+        $subnet_str = implode('.', $subnet_array);
+
+        //Length of the string we are reviewing 
+        $charNum = count($subnet_str);
+
+        //Creating array of subnets that are in use
+        $inRange =array();
+
+        //This loops through the ip addresses given by @index
+        //and checks those that match the given subnet address 
+        foreach ($ip_addresses as $ip_address){
+
+            $ip_substr = substr($ip_address, 0, 9);
+
+
+            if ($ip_substr === $subnet_str){
+
+                array_push($inRange, $ip_address);
+            }
+        }
+
+
+        //creates next ip address available and returns it
+        for ($i = 1; $i < 255; $i ++){
+
+            $next = $subnet_str . '.' . $i;
+
+            if (!in_array($next, $inRange)){
+                return $next;
+            }
+        }
 
 
     }
